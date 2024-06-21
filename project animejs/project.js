0b051f35006f535461bfce8645ae93542ed4565d
sayin_stuff <58088965+charlielikescakeandstuff@users.noreply.github.com>
2024-06-21 19:07:21 +0200

    Add files via upload

---
+++ project animejs/project.js
@@ -0,0 +1,14 @@
+let anime = function(){
+    anime({
+        targets: '.square',
+        left: '900px',
+        translateY: 250,
+        backgroundColor: '#E3D985',
+        borderRadius: ['0%', '50%'],
+        easing: 'easeInOutQuad',
+        loop:true
+      })
+}
+addEventListener('click',anime)
+    
+
